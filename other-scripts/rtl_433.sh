a852bc3ec678206b363ac5aa69119371d80c7e22
Christian Schrötter <cs@fnx.li>
2017-06-04 12:44:49 +0200

    Workaround for rtl_433/#136

--- other-scripts/rtl_433.sh
+++ other-scripts/rtl_433.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 while true
 do
-	# Only an example! Adjust to your needs...
-	rtl_433 -R 50 -l 1 -g 50 -F json -T 900 -q \
-	2>/dev/null | ism-catcher --live; sleep 10
+	# Only a simple example. Adjust to your needs...
+	#
+	# Timeout is required as workround for issue #136:
+	# https://github.com/merbanan/rtl_433/issues/136
+	timeout 1000 \
+	rtl_433 -R 50 -l 1 -g 50 -F json -T 900 -q 2>/dev/null \
+		| ism-catcher --live; sleep 10
 done
 
 # Copy everything to a logfile: (don't forget to setup logrotate if required!)
