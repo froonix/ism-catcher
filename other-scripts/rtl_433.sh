fb468c8a7ceadd295ac2b563c46ab82a24b5cdd6
Christian Schrötter <cs@fnx.li>
2017-06-04 15:27:54 +0200

    ...

--- other-scripts/rtl_433.sh
+++ other-scripts/rtl_433.sh
@@ -2,12 +2,8 @@
 while true
 do
 	# Only a simple example. Adjust to your needs...
-	#
-	# Timeout is required as workround for issue #136:
-	# https://github.com/merbanan/rtl_433/issues/136
-	timeout 1000 \
-	rtl_433 -R 50 -l 1 -g 50 -F json -T 900 -q 2>/dev/null \
-		| ism-catcher --live; sleep 30
+	rtl_433 -R 50 -l 1 -g 50 -F json -T 500 -q 2>/dev/null \
+		| ism-catcher --live; sleep 10
 done
 
 # Copy everything to a logfile: (don't forget to setup logrotate if required!)
