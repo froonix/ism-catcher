59c4220d96864914b3d0285bc8fd49783fe414e9
Christian Schrötter <cs@fnx.li>
2017-06-07 10:59:05 +0200

    Redirect STDERR to logfile

--- other-scripts/rtl_433.sh
+++ other-scripts/rtl_433.sh
@@ -2,12 +2,12 @@
 while true
 do
 	# Only a simple example. Adjust to your needs...
-	rtl_433 -R 50 -l 1 -g 50 -F json -T 500 -q 2>/dev/null \
+	rtl_433 -R 50 -l 1 -g 50 -F json -T 500 -q \
 		| ism-catcher --live; sleep 10
 done
 
 # Copy everything to a logfile: (don't forget to setup logrotate if required!)
-# rtl_433 […] 2>/dev/null | tee -a ~/path/to/archive.json | ism-catcher --live
+# rtl_433 […] 2> ./error.log | tee -a ./archive.json | ism-catcher --live
 
 # Copy each line to STDERR for debugging purposes:
 # rtl_433 […] | tee >(cat >&2) | ism-catcher --live
