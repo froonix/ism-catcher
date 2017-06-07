7a8144371fcae1b1aa2ff7d8201da7bb7e37aa26
Christian Schrötter <cs@fnx.li>
2017-06-07 11:15:31 +0200

    ...

--- other-scripts/rtl_433.sh
+++ other-scripts/rtl_433.sh
@@ -7,7 +7,7 @@ do
 done
 
 # Copy everything to a logfile: (don't forget to setup logrotate if required!)
-# rtl_433 […] 2> ./error.log | tee -a ./archive.json | ism-catcher --live
+# rtl_433 […] 2>> ./error.log | tee -a ./archive.json | ism-catcher --live
 
 # Copy each line to STDERR for debugging purposes:
 # rtl_433 […] | tee >(cat >&2) | ism-catcher --live
