fedb84f07f350d3846174f6b20c677ad8744d375
Christian Schrötter <cs@fnx.li>
2022-01-12 12:31:38 +0100

    Update rtl_433.sh sample script

--- other-scripts/rtl_433.sh
+++ other-scripts/rtl_433.sh
@@ -2,15 +2,12 @@
 while true
 do
 	# Only a simple example. Adjust to your needs...
-	rtl_433 -R 50 -l 1 -g 50 -F json -T 500 -q \
-		| ism-catcher --live
-
-	if [[ ${PIPESTATUS[0]} -ne 0 ]]
-	then sleep 30; else sleep 10; fi
+	rtl_433 -F json -g 20 -Y autolevel | ism-catcher --live
+	[[ ${PIPESTATUS[0]} -ne 0 ]] && sleep 30 || sleep 10
 done
 
 # Copy everything to a logfile: (don't forget to setup logrotate if required!)
-# rtl_433 […] 2>> ./error.log | tee -a ./archive.json | ism-catcher --live
+# rtl_433 -F json -g 20 -Y autolevel -M level 2>> ./error.log | tee -a ./archive.json | ism-catcher --live
 
 # Copy each line to STDERR for debugging purposes:
-# rtl_433 […] | tee >(cat >&2) | ism-catcher --live
+# rtl_433 -F json -g 20 -Y autolevel -M level | tee >(cat >&2) | ism-catcher --live
